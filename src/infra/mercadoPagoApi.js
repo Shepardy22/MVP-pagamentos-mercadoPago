212a60eb000bb7803bb15b1f339de64a470ff66a
Shepardy22 <inv72kmaster@gmail.com>
2026-02-11 21:48:14 -0300

    remoção de chaves desnecessárias na função buscarPagamentosPendentes

--- src/infra/mercadoPagoApi.js
+++ src/infra/mercadoPagoApi.js
@@ -9,8 +9,6 @@ export async function buscarPagamentosPendentes(cpf) {
   const resposta = await api.get(`/pendentes${cpf ? `?cpf=${cpf}` : ''}`);
   return resposta.data;
 }
-}
-}
 
 export async function cancelarPagamento(id) {
   const resposta = await api.put(`/${id}`);
